9b7e4498f35508a2b3e9ed5a618f430848f4d1f2
otukutun <orehaorz@gmail.com>
2014-04-13 21:46:37 +0900

    regexpの練習

---
+++ regexp.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env ruby
+#conding: utf-8
+
+#prac1
+mail_address = "test@gmail.com"
+/^(.+)@(.+)$/ =~ mail_address
+
+local_part = $1
+domein_part = $2
+p local_part, domein_part
+
+#prac2
+
+str = "オブジェクト指向は難しい！なんて難しいんだ！"
+p str.gsub(/難しいんだ/,"簡単なんだ").gsub(/難しい/,"簡単だ")
+
+#prac3
+
+def word_capitalize(str)
+  str.split(/\-/)
+  #str.split(/\-/).collect do |w|
+  #  w.capitalize
+  #end.join('-')
+end
+
+p word_capitalize("test-aappp-fjoeia-few")
